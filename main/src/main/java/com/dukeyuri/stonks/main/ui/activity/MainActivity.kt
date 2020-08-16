a742c499d3ba792b9865754c02e07e932eee598f
Yuri <dukeyuri.dev@gmail.com>
2020-08-16 20:03:27 +0300

    Added main module

---
+++ main/src/main/java/com/dukeyuri/stonks/main/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dukeyuri.stonks.main.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dukeyuri.stonks.main.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
